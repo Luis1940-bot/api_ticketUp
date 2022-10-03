f0bba681cccf6a9c886cca7615902e8ec98e84a8
Luis Gimenez <luisconsultor@outlook.com>
2022-10-03 15:33:27 -0500

    commit se agrega criticidad

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,11 +5,13 @@ const users = require("./users");
 const areas = require("./areas");
 const functions = require("./functions");
 const categorias = require("./categorias");
+const criticidad = require("./criticidad");
 
 //Routes and middlewares
 router.use("/", users);
 router.use("/", areas);
 router.use("/", functions);
 router.use("/", categorias);
+router.use("/", criticidad);
 
 module.exports = router;
