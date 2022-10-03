449b4f41aa5a883432e6d25dcbcea98385e9e9a4
Luis Gimenez <luisconsultor@outlook.com>
2022-10-03 15:15:02 -0500

    commit se agrega categorias

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,10 +4,12 @@ const router = require("express").Router();
 const users = require("./users");
 const areas = require("./areas");
 const functions = require("./functions");
+const categorias = require("./categorias");
 
 //Routes and middlewares
 router.use("/", users);
 router.use("/", areas);
 router.use("/", functions);
+router.use("/", categorias);
 
 module.exports = router;
